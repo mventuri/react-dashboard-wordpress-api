525dd5781d538aa9c3f9c33655687cd69a885e2b
Marco Venturi <mventuri10@gmail.com>
2022-02-20 22:11:56 +0100

    Code refactoring after opened issue

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -2,60 +2,63 @@ import React from 'react';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table'
 
-class Report extends React.Component {
-  
+class Report extends React.Component { 
+  constructor(props) { 
+      super(props); 
+      this.state = { countActiveState: 0, countInactiveState: 0, };
+  } 
+
   componentDidMount() {
-    let countActive  = 0;
-    let countInactive = 0;
-    
-    axios.get("https://<BASE_URL>/wp-json/wp/v2/plugins", {
-      auth: {
-        username: process.env.REACT_APP_USERNAME,
-        password: process.env.REACT_APP_CLIENT_SECRET
-      }
-    })
-      .then(res => {
-        const plugins = res.data;
-        for(let key in plugins) {
-          if(plugins[key].status === "active") {
-            countActive++;
-          }
-          else{
-            countInactive++;
-          }
-      }
-      document.getElementById("activePlugin").innerHTML = countActive;
-      document.getElementById("inactivePlugin").innerHTML = countInactive;
-      })
-      .catch(error => {
-        alert("Something went wrong. Try again later.");
-        console.log(error);
-     })
-    }
+  let countActive  = 0;
+  let countInactive = 0;
   
-  render() {
-    return (
-      <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>Status</th>
-      <th>Plugin Amount</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th>Active</th>
-      <td id="activePlugin"></td>
-    </tr>
-    <tr>
-      <th>Inactive</th>
-      <td id="inactivePlugin"></td>
-    </tr>
-  </tbody>
-</Table>
-    )
+  axios.get("<BASE_URL>/wp-json/wp/v2/plugins", {
+    auth: {
+      username: process.env.REACT_APP_USERNAME,
+      password: process.env.REACT_APP_CLIENT_SECRET
+    }
+  })
+  .then(res => {
+      const plugins = res.data;
+      for(let key in plugins) {
+        if(plugins[key].status === "active") {
+          countActive++;
+          this.setState({countActiveState: countActive}); 
+        }
+        else{
+          countInactive++;
+          this.setState({countInactiveState: countInactive}); 
+        }
+    }
+    })
+    .catch(error => {
+      alert("Something went wrong. Try again later.");
+      console.log(error);
+   })
   }
-}
 
-export default Report
+  render() { 
+      return ( 
+          <Table striped bordered hover >
+              <thead>
+                  <tr>
+                  <th>Status</th>
+                  <th>Plugin Amount</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                  <th>Active</th>
+                  <td>{this.state.countActiveState}</td>
+                  </tr>
+                  <tr>
+                  <th>Inactive</th>
+                  <td>{this.state.countInactiveState}</td>
+                  </tr>
+              </tbody>
+          </Table>
+      ); 
+  } 
+} 
 
+export default Report;
